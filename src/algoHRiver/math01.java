95dade4b286f49514de520661828053c362009d6
HRiver <sanscout@naver.com>
2023-12-05 10:07:33 +0900

    4375

---
+++ src/algoHRiver/math01.java
@@ -0,0 +1,24 @@
+
+//https://www.acmicpc.net/problem/4375
+//백준은 클래스 명이 Main 이어야 돌아감 ..
+import java.util.*;
+public class math01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int n = sc.nextInt();
+            int num =0;
+
+            for (int i=1;; i++) {
+                num = num*10 + 1;
+                num %= n;
+                if(num ==0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+
+}
