cbdae68fcc5329958610f56df2c02ac352a27af4
HRiver <sanscout@naver.com>
2023-12-05 16:55:10 +0900

    gugudansolve

--- src/algoHRiver/Main.java
+++ src/algoHRiver/Main.java
@@ -1,3 +1,5 @@
+//https://jungol.co.kr/problem/1291?cursor=eyJwcm9ibGVtc2V0Ijo2LCJmaWVsZCI6MCwiaWR4IjowfQ%3D%3D
+
 package algoHRiver;
 
 import java.util.*;
@@ -5,14 +7,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        boolean flag = true;
         int s = sc.nextInt();
         int e = sc.nextInt();
+        while(flag) {
+
+            if (s < 2 || s > 9 || e < 2 || e > 9) {
+                System.out.println("INPUT ERROR!");
+                s = sc.nextInt();
+                e = sc.nextInt();
+            }
+            else{
+                flag=false;
+            }
+        }
         if (s>=e){
+
             for(int j=1;j<10; j++){
+
                 for(int i=s; i>=e; i--){
+                    if(i*j<10){
+                        System.out.print(i+" * "+j+" =  "+(i*j)+"   ");
 
-                    System.out.print(i+" * "+j+" = "+(i*j)+"   ");
-
+                    }
+                    else {
+                        System.out.print(i + " * " + j + " = " + (i * j) + "   ");
+                    }
                 }
                 System.out.print("\n");
             }
@@ -20,10 +40,13 @@ public class Main {
 
         else {
             for(int j=1;j<10; j++){
-                for(int i=s; i<=e; i--){
+                for(int i=s; e>=i; i++) {
+                    if (i * j < 10) {
+                        System.out.print(i + " * " + j + " =  " + (i * j) + "   ");
 
-                    System.out.print(i+" * "+j+" = "+(i*j)+"   ");
-
+                    } else {
+                        System.out.print(i + " * " + j + " = " + (i * j) + "   ");
+                    }
                 }
                 System.out.print("\n");
             }
