7193d49c4054a8b7a576c7a9fcf7ae67ad36d5e5
HRiver <sanscout@naver.com>
2023-12-08 14:58:30 +0900

    20231208

---
+++ src/algoHRiver/brute03.java
@@ -0,0 +1,46 @@
+package algoHRiver;
+//https://gyuwon95.tistory.com/48
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class brute03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+
+        int a =Integer.parseInt(st.nextToken());
+        int b =Integer.parseInt(st.nextToken());
+        int c =Integer.parseInt(st.nextToken());
+
+        int e=1;
+        int s=1;
+        int m=1;
+        int cnt=1;
+
+        while(true){
+            if(a==e && b==s && c==m ){
+                System.out.println(cnt);
+                bw.flush();
+                break;
+            }
+
+            cnt++;
+            e++;
+            s++;
+            m++;
+
+            if(e==16){
+                e=1;
+            }
+            if (s==29){
+                s=1;
+            }
+            if(m==20){
+                m=1;
+            }
+        }
+
+    }
+}
