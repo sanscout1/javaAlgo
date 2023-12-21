f4530ea17160af50267f4e802bd08dd61978762c
HRiver <sanscout@naver.com>
2023-12-22 08:40:38 +0900

    update P1107

--- src/algoHRiver/Baekjoon/Remote1107/P1107.java
+++ src/algoHRiver/Baekjoon/Remote1107/P1107.java
@@ -2,44 +2,88 @@ package algoHRiver.Baekjoon.Remote1107;
 
 // https://www.acmicpc.net/problem/1107
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class P1107 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
         int buttonClick = 1000000;
-        String endChannel = in.nextLine();
+        int endChannel = in.nextInt();
+        int startChannel = 100;
+        int anslength=0;
+        boolean[] brokenButtons = new boolean[10];
+
         int brokenButtonTotal = in.nextInt();
-        StringBuilder brokenButton = new StringBuilder();
-
-        // 박살난 버튼 저장
         for (int i = 0; i < brokenButtonTotal; i++) {
-            brokenButton.append(in.next());
+            brokenButtons[in.nextInt()]=true;
         }
 
-        for (int i = 0; i < 1000000; i++) {
-            boolean Flag = false;
-            if (endChannel.equals("100")) {
-                buttonClick = 0;
-                break;
+        for (int i = 0; i < 1000001; i++) {
+            if(startChannel==endChannel){
+                buttonClick=0;
             }
-            for (int j = 0; j < brokenButton.length(); j++) {
-                StringBuilder tmpSB = new StringBuilder();
-                tmpSB.append(brokenButton.charAt(j));       //StringBuilder 의 charsequence 는 char로 어케 수정할것인가  // 이게 최선인가? (비교할 숫자 )
-                if ((String.valueOf(i).contains(tmpSB))) {    // 여기서 왜 i 를 원하는 값을 걸러내지 못하냐  // 잘걸러내더라     하나라도 박살난 버튼이 있다면 다음 수로
-                    Flag = true;
+            boolean flag = true;
+            int tmpNum =i;
+            while(tmpNum>0){
+                if(brokenButtons[tmpNum % 10]){
+                    flag = false;
+                    break;
                 }
+                tmpNum/=10;
             }
-            if (!Flag) {                // 박살난 버튼 숫자가 없다면
-                int tmpAbs = Math.abs(Integer.parseInt(endChannel) - i);
-                if (buttonClick > tmpAbs) {
-                    buttonClick = tmpAbs;
+            if(flag){
+                tmpNum = Math.abs(endChannel-i);
+                if(buttonClick>tmpNum){
+                    buttonClick=tmpNum;
+
+                    anslength=String.valueOf(i).length();
                 }
             }
         }
-        buttonClick += endChannel.length();
-        System.out.println(buttonClick);   //백준 입력 예시 다 맞게 출력되지만 제출하면 틀림,,,,,
+        buttonClick+=anslength;
+        System.out.println(buttonClick);
+
+
+
+//        int buttonClick = 1000000;
+//        String minOfBtn ="";
+//        String endChannel = in.nextLine();
+//        int brokenButtonTotal = in.nextInt();
+//        StringBuilder brokenButton = new StringBuilder();
+//        //long startTime = System.currentTimeMillis();
+//        // 박살난 버튼 저장
+//        for (int i = 0; i < brokenButtonTotal; i++) {
+//            brokenButton.append(in.next());
+//        }
+//
+//        for (int i = 0; i < 1000001; i++) {
+//            boolean Flag = false;
+//            if (endChannel.equals("100")) {
+//                buttonClick = 0;
+//                break;
+//            }
+//            for (int j = 0; j < brokenButton.length(); j++) {
+//                StringBuilder tmpSB = new StringBuilder();
+//                tmpSB.append(brokenButton.charAt(j));       //StringBuilder 의 charsequence 는 char로 어케 수정할것인가  // 이게 최선인가? (비교할 숫자 )
+//                if ((String.valueOf(i).contains(tmpSB))) {    // 여기서 왜 i 를 원하는 값을 걸러내지 못하냐  // 잘걸러내더라     하나라도 박살난 버튼이 있다면 다음 수로
+//                    Flag = true;
+//                }
+//            }
+//            if (!Flag) {                // 박살난 버튼 숫자가 없다면
+//                int tmpAbs = Math.abs(Integer.parseInt(endChannel) - i);
+//                if (buttonClick > tmpAbs) {
+//                    buttonClick = tmpAbs;
+//                    minOfBtn= String.valueOf(i);
+//                }
+//            }
+//        }
+//        buttonClick += minOfBtn.length();  //1221 오류 발견 숫자 하나씩 더 추가 될 경우를 발견 // 어째서 수정해도 틀리는 것인가?
+//        System.out.println(buttonClick);   //백준 입력 예시 다 맞게 출력되지만 제출하면 틀림,,,,,
+//        //long endTime = System.currentTimeMillis();
+//        //System.out.println(endTime-startTime);
     }
 
 }
