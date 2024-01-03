21c951c3b6f46ae483c1c1b15c05f707b9f777d3
HRiver <sanscout@naver.com>
2024-01-03 13:56:25 +0900

    P15659 solved

--- src/algoHRiver/Baekjoon/NM15649/P15649.java
+++ src/algoHRiver/Baekjoon/NM15649/P15649.java
@@ -5,46 +5,38 @@ import java.util.Scanner;
 
 //https://www.acmicpc.net/problem/15649
 public class P15649 {
+    static boolean[] tmpBool = new boolean[11];
+    static int[] tmpNum  = new int[11];
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         int n= in.nextInt();
         int m = in.nextInt();
-        int print = m;
-        int[] tmpArray = new int[m];
         int index=0;
-        recur(tmpArray,index,n,m,print);
+        recur(index,n,m);
+
     }
 
+    public static void recur(int index,int n ,int m){
+        if(index==m){
 
-    public static void recur(int[] tmpArray,int index,int n,int m,int print){
-        boolean flag=false;
-        if(m>0) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 0; j <=index ; j++) {
-                    if(tmpArray[j]==i){
-                        flag=true;
-                    }
-                }
-                if(!flag) {
-                    m--;
-                    tmpArray[index] = i;
-                    index++;
-                    recur(tmpArray, index, n, m, print);
-                    index--;
-                    m++;
-                }
-                flag=false;
-            }
-        }
-        if(m==0){
-            for (int i = 0; i < print; i++) {
-                System.out.print(tmpArray[i]+" ");
+            for (int i = 0; i < index; i++) {
+                System.out.print(tmpNum[i]+" ");
             }
             System.out.println();
         }
+        else if(index<m){
+            for (int i = 1; i <= n; i++) {
+                if(tmpBool[i]) continue;
+                tmpBool[i]=true;
+                tmpNum[index]=i;
+                index++;
+                recur(index,n,m);
+                index--;
+                tmpBool[i]=false;
+            }
+
+        }
     }
-
-
 
 }
